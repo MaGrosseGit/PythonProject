c41dd20f9135b33235a22811b1fd6c5cb8049b25
Benjamin <benjamin.domingues@me.com>
2015-05-09 02:55:24 +0200

    Maj Motor
    rangement et utilisation d'une class par tag/type d'asset

--- Motor.py
+++ Motor.py
@@ -4,29 +4,51 @@ tagWindow = "Window"
 tagWindowAsset = "Cube001"
 tagDoor = "Door"
 
+class AssetType:
+	"""Classe pour chaque type d'asset:
+	- La liste d'asset disponible
+	- La liste d'empty pour les placer
+	- le nombre d'empty
+	- le tag des assets"""
 
-def PlaceObject(obj,target):
-    obj.location = target.location
-    obj.rotation_euler = target.rotation_euler
+    
+	def __init__(self, tag):
+		"""Constructeur de notre classe"""
+		        
+		self.tag = tag
+		self.listTargets = [obj for obj in bpy.context.scene.objects if obj.name.startswith(self.tag)]
+		self.nbtarget = len(self.listTargets)
+		self.listAssets = self.ChooseAssets()
+        
 
-def ChooseAssets(nbAsset,tag): #Add asset's list instead of cube creation process
-	assets = []
-	for i in range(0,nbAsset) :
-		bpy.ops.mesh.primitive_cube_add()
-		mesh = bpy.context.object
-		mesh.name = tagWindowAsset + ".001"
-		assets.append(mesh)
-	return assets
+	def ChooseAssets(self): #Add asset's list instead of cube creation process
+		assets = []
+		for i in range(0,self.nbtarget) :
+			bpy.ops.mesh.primitive_cube_add()
+			mesh = bpy.context.object
+			mesh.name = self.tag+ "_asset.001"
+			assets.append(mesh)
+		return assets   
+
+	def PlaceObject(self, obj,target):
+		obj.location = target.location
+		obj.rotation_euler = target.rotation_euler  
+		
+	def PlaceAllObjects(self):
+		for i in range(0,self.nbtarget) :
+			self.PlaceObject(self.listAssets[i],self.listTargets[i])
+        
+	
 
 
-scene = bpy.context.scene
-window_targets= [obj for obj in scene.objects if obj.name.startswith(tagWindow)]
-window_objs = ChooseAssets(len(window_targets),tagWindowAsset)
-print (window_objs)
+windows = AssetType(tagWindow)
+windows.PlaceAllObjects()
 
-if len(window_targets) == len(window_objs) :
-	for i in range(0,len(window_targets)) :
-		PlaceObject(window_objs[i],window_targets[i])
+
+
+
+
+
 
 
 
