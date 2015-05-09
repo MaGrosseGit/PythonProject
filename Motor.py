8e3530c5882111b4e9feec32f810fea875a6e53b
Benjamin <benjamin.domingues@me.com>
2015-05-09 17:12:47 +0200

    Fonctional Motor
    utilise une list d'asset qu'il duplique et met en place à la bonne
    taille

--- Motor.py
+++ Motor.py
@@ -1,8 +1,34 @@
 import bpy
+import random
 
 tagWindow = "Window"
-tagWindowAsset = "Cube001"
+tagWindowAsset = "Window_asset"
 tagDoor = "Door"
+uniqueAssets = False # If you what different asset on each empty
+
+
+
+def duplicateObject(scene, name, copyobj):
+ 
+    # Create new mesh
+    mesh = bpy.data.meshes.new(name)
+ 
+    # Create new object associated with the mesh
+    ob_new = bpy.data.objects.new(name, mesh)
+ 
+    # Copy data block from the old object into the new object
+    ob_new.data = copyobj.data.copy()
+    ob_new.scale = copyobj.scale
+    ob_new.location = copyobj.location
+ 
+    # Link new object to the given scene and select it
+    scene.objects.link(ob_new)
+    ob_new.select = True
+ 
+    return ob_new
+
+
+
 
 class GenerateType:
 	"""Classe pour chaque type d'asset:
@@ -18,40 +44,40 @@ class GenerateType:
 		self.tag = tag
 		self.listTargets = [obj for obj in bpy.context.scene.objects if obj.name.startswith(self.tag)]
 		self.nbtarget = len(self.listTargets)
-		self.listAssets = self.ChooseAssets()
+		self.listAssets = [obj for obj in bpy.context.scene.objects if obj.name.startswith("Asset_" + self.tag)] #Change with loaded list
+		print (len(self.listAssets))
+		self.listChoosenAssets = self.ChooseAssets()
+		print (len(self.listChoosenAssets))
         
 
-	def ChooseAssets(self): #Add asset's list instead of cube creation process
+	def ChooseAssets(self):
 		assets = []
+		tempList = []
 		for i in range(0,self.nbtarget) :
-			bpy.ops.mesh.primitive_cube_add()
-			mesh = bpy.context.object
-			mesh.name = self.tag+ "_asset.001"
+			tempInt = random.randint(0,len(self.listAssets)-1)
+			if uniqueAssets :
+				while tempInt in tempList :
+					tempInt = random.randint(0,len(self.listAssets)-1)
+				tempList.append(tempInt)
+			mesh = duplicateObject(bpy.context.scene, "Asset_" + self.tag+ ".001", self.listAssets[tempInt])
+			
 			assets.append(mesh)
 		return assets   
 
 	def PlaceObject(self, obj,target):
 		obj.location = target.location
 		obj.rotation_euler = target.rotation_euler  
+		obj.dimensions = target.scale *2
 		
 	def PlaceAllObjects(self):
 		for i in range(0,self.nbtarget) :
-			self.PlaceObject(self.listAssets[i],self.listTargets[i])
+			self.PlaceObject(self.listChoosenAssets[i],self.listTargets[i])
         
 	
 
 
 windows = GenerateType(tagWindow)
 windows.PlaceAllObjects()
-
-doors = GenerateType(tagDoor)
-doors.PlaceAllObjects()
-
-
-
-
-
-
 
 
 
